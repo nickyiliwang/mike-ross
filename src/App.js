632f7f46d6b073c8e19914b4c255025c29d6e8dd
Nick Wang <nickyiliwang@gmail.com>
2020-01-28 15:04:10 -0500

    basic folder structure

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+class App extends React.Component {
+  onClick = () => alert("click");
+
+  render() {
+    return <div>What</div>;
+  }
+}
+
+export default App;
