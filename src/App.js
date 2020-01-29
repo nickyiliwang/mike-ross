85987b5e16106f913a54cfc62f4f230092989d8b
Nick Wang <nickyiliwang@gmail.com>
2020-01-28 19:06:03 -0500

    routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
 import "./setup.css";
 import "./App.css";
+import Header from "./components/Header";
 import Search from "./components/Search";
 import DisplayDocuments from "./components/DisplayDocuments";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <Router>
-      <Search />
-      <DisplayDocuments />
+      <div className="wrapper">
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Search />
+            <DisplayDocuments />
+          </Route>
+        </Switch>
+      </div>
     </Router>
   );
 };
